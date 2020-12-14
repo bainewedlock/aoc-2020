120d029938ccd6fd918c694d6a0f39af3c989f0b
boeckwi <w.boeck@walz.de>
2020-12-14 11:04:17 +0100

    day 12 refactoring

--- 12/Solution.fs
+++ 12/Solution.fs
@@ -4,6 +4,10 @@ type Ship = {
     location    : int * int
     waypoint    : int * int }
 
+type Transformation =
+    | ChangeLocation of (int * int)
+    | ChangeWaypoint of (int * int)
+
 let parse (input:string) =
     input.Split '\n'
     |> Array.toList
@@ -14,10 +18,7 @@ let rec rotateVector90 (x0, y0) n =
     if n = 0 then (x0, y0)
     else rotateVector90 (y0, -x0) (n-1)
 
-let rec moveVector (x0, y0) (dx, dy) = function
-    | 0            -> x0, y0
-    | n when n > 0 -> moveVector (x0+dx, y0+dy) (dx, dy) (n-1)
-    | x            -> failwithf "unexpected: %A" x
+let moveVector (x0, y0) factor (dx, dy) = x0+dx*factor, y0+dy*factor
 
 let tryParseOrientation = function
     | 'N' -> Some ( 0,  1)
@@ -31,43 +32,45 @@ let tryParseRotation = function
     | 'L', degree -> Some (4 - degree / 90)
     | _           -> None
 
-let applyShipOffset (cmd, n) ship =
-    match tryParseOrientation cmd with
-    | Some speed -> { ship with location = moveVector ship.location speed n }
-    | None       -> ship
+let tryParseForward = function
+    | 'F', speed -> Some speed
+    | _          -> None
 
-let applyRotation cmd ship =
-    match tryParseRotation cmd with
-    | Some r -> { ship with waypoint = rotateVector90 ship.waypoint r }
-    | None   -> ship
+let offsetShip (cmd, n) ship =
+    tryParseOrientation cmd
+    |> Option.map (moveVector ship.location n)
+    |> Option.map ChangeLocation
 
-let applyForward (cmd, n) ship =
-    match cmd with
-    | 'F' -> { ship with location = moveVector ship.location ship.waypoint n }
-    | _   -> ship
+let offsetWaypoint (cmd, n) ship =
+    tryParseOrientation cmd
+    |> Option.map (moveVector ship.waypoint n)
+    |> Option.map ChangeWaypoint
 
-let applyWaypointOffset (cmd, n) ship =
-    match tryParseOrientation cmd with
-    | Some speed -> { ship with waypoint = moveVector ship.waypoint speed n }
-    | None       -> ship
+let rotateWaypoint cmd ship =
+    tryParseRotation cmd
+    |> Option.map (rotateVector90 ship.waypoint)
+    |> Option.map ChangeWaypoint
 
-let part1Actions ship a =
-    ship
-    |> applyShipOffset a
-    |> applyForward a
-    |> applyRotation a
+let moveForward cmd ship =
+    tryParseForward cmd
+    |> Option.map (fun n -> moveVector ship.location n ship.waypoint)
+    |> Option.map ChangeLocation
 
-let part2Actions ship a =
-    ship
-    |> applyWaypointOffset a
-    |> applyForward a
-    |> applyRotation a
+let private apply cmd ship f = 
+    match f cmd ship with
+    | None -> ship
+    | Some (ChangeLocation x) -> { ship with location = x }
+    | Some (ChangeWaypoint x) -> { ship with waypoint = x }
 
-let genericSolve actions wp =
+let private genericSolve handlers wp =
     parse
-    >> List.fold actions { waypoint = wp; location = 0, 0 }
+    >> List.fold handlers { waypoint = wp; location = 0, 0 }
     >> fun { location = x, y } -> x, y, abs x + abs y
 
-let solve  = genericSolve part1Actions  ( 1, 0)
-let solve2 = genericSolve part2Actions (10, 1)
+let genericExecute actions ship cmd = List.fold (apply cmd) ship actions
+let execute1 = genericExecute [ moveForward; rotateWaypoint; offsetShip ]
+let execute2 = genericExecute [ moveForward; rotateWaypoint; offsetWaypoint ]
+
+let solve  = genericSolve execute1 ( 1, 0)
+let solve2 = genericSolve execute2 (10, 1)
 
--- 12/Tests.fs
+++ 12/Tests.fs
@@ -15,44 +15,44 @@ let all =
             test "demoinput" { solve2 demoinput =! (214, -72, 286)} ]
         testList "move waypoint" [
             test "north" {
-                part2Actions (swp 10 1) ('N', 3) =!
+                execute2 (swp 10 1) ('N', 3) =!
                     {   location = 0, 0
                         waypoint = 10, 4 } } ]
         testList "solve" [
             test "demoinput" { solve demoinput =! (17, -8, 25) } ]
         testList "actions" [
             test "forward" {
-                part1Actions (swp 10 1) ('F', 10) =!
+                execute1 (swp 10 1) ('F', 10) =!
                     {   location = 100, 10
                         waypoint = 10, 1 }
-                part1Actions (swp 2 1) ('F', 2) =!
+                execute1 (swp 2 1) ('F', 2) =!
                     {   location = 4, 2
                         waypoint = 2, 1 }
-                part1Actions (swp -1 0) ('F', 1) =!
+                execute1 (swp -1 0) ('F', 1) =!
                     {   location = -1, 0
                         waypoint = -1, 0 } }
             test "turns" {
-                part1Actions (swp 1 0) ('R', 90) =! {
+                execute1 (swp 1 0) ('R', 90) =! {
                     waypoint    = 0, -1
                     location    = 0, 0 }
-                part1Actions (swp 0 1) ('R', 90) =! {
+                execute1 (swp 0 1) ('R', 90) =! {
                     location    = 0, 0 
                     waypoint    = 1, 0 }
-                part1Actions (swp -1 0) ('R', 180) =! {
+                execute1 (swp -1 0) ('R', 180) =! {
                     location    = 0, 0
                     waypoint    = 1, 0 } }
             test "simple moves" {
                 let ship = { location = 0,0; waypoint = 0,0 }
-                part1Actions ship ('N', 1) =! {
+                execute1 ship ('N', 1) =! {
                     waypoint    = 0, 0
                     location    = 0, 1 }
-                part1Actions ship ('E', 1) =! {
+                execute1 ship ('E', 1) =! {
                     waypoint    = 0, 0
                     location    = 1, 0 }
-                part1Actions ship ('W', 1) =! {
+                execute1 ship ('W', 1) =! {
                     waypoint    = 0, 0
                     location    = -1, 0 }
-                part1Actions ship ('S', 1) =! {
+                execute1 ship ('S', 1) =! {
                     waypoint    = 0, 0
                     location    = 0, -1 } } ]
         testList "parse" [
