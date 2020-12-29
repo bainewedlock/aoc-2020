cf7b5518cd589623fe279d5c50155f5558696583
boeckwi <w.boeck@walz.de>
2020-12-29 14:50:50 +0100

    day 20 part 2 started

--- 20/Solution2.fs
+++ 20/Solution2.fs
@@ -1,3 +1,24 @@
 ﻿module Solution2
 
-
+let rec arrange (tiles:Map<int, int Set>)  =
+    let size = tiles.Count |> float |> sqrt |> int
+    let rec loop (tiles:Map<int, int Set>) (acc:Map<int * int, int>) =
+        if tiles.IsEmpty then Some acc else
+        let x = acc.Count % size
+        let y = acc.Count / size
+        let refTile = 
+            if x = 0 then acc.[0, y-1]
+            else          acc.[x-1, y]
+        tiles
+        |> Map.toSeq
+        |> Seq.tryPick (fun (no, others) ->
+            if not <| others.Contains refTile then None else
+            let acc' = acc.Add((x,y), no)
+            let tiles' = tiles.Remove(no) 
+            loop tiles' acc')
+    let someCorner =
+        tiles |> Map.toSeq |> Seq.find (snd >> Set.count >> (=)2) |> fst
+    let tilesWithoutCorner = tiles.Remove someCorner
+    let initialArrangement = Map [(0,0), someCorner]
+    loop tilesWithoutCorner initialArrangement
+    |> Option.get
--- 20/Tests2.fs
+++ 20/Tests2.fs
@@ -4,48 +4,37 @@ open Swensen.Unquote
 let  utest = test
 open Expecto
 
+open Solution
 open Solution2
 open Demoinput
 
 
-//let fitTalesH t1 t2 = t1.Edges.[Right] = t2.Edges.[Left]
-//let fitTalesV t1 t2 = t1.Edges.[Bottom] = t2.Edges.[Top]
+let fitTilesH t1 t2 = t1.Edges.[Right] = t2.Edges.[Left]
+let fitTilesV t1 t2 = t1.Edges.[Bottom] = t2.Edges.[Top]
 
-let x : int[,] = Array2D.create 2 2 22
-
-let arrange tiles =
-    Array2D.init 2 2 (fun x y -> tiles |> List.item (y*2 + x))
-
-//parse input
-
-//[<Tests>]
-//let part2 =
-//    testList "part2" [
-//        ptestList "arrange" [
-//            test "simple 2x2 example without transformation" {
-//                let t1 = { Edges = Map [Right, "###"; Bottom, "###"]}
-//                let t2 = { Edges = Map [Left,  "###"]}
-//                let t3 = { Edges = Map [Top,  "###"; Right, "###"]}
-//                let t4 = { Edges = Map [Left,  "###"]}
-//                let matrix = arrange [t1;t3;t2;t4]
-//                matrix.[0,0] =! t1
-//                matrix.[1,0] =! t2
-//                matrix.[0,1] =! t3
-//                matrix.[1,1] =! t4
-//            }
-//        ]
-//        testList "fitTiles" [
-//            test "vertical" {
-//                let t1 = { Edges = Map [Bottom, "###"]}
-//                let t2 = { Edges = Map [Top,  "###"]}
-//                let t3 = { Edges = Map [Top,  "##."]}
-//                fitTalesV t1 t2 =! true
-//                fitTalesV t1 t3 =! false }
-//            test "horizontal" {
-//                let t1 = { Edges = Map [Right, "###"]}
-//                let t2 = { Edges = Map [Left,  "###"]}
-//                let t3 = { Edges = Map [Left,  "##."]}
-//                fitTalesH t1 t2 =! true
-//                fitTalesH t1 t3 =! false }
-//        ]
-//    ]
+[<Tests>]
+let part2 =
+    testList "part2" [
+        testList "arrange" [
+            test "demoinput" {
+                let matrix = arrange (parse demoinput |> analyze)
+                matrix
+                |> Map.toList
+                |> List.map snd
+                |> List.distinct
+                |> List.length =! 9 } ]
+        testList "fitTiles" [
+            test "vertical" {
+                let t1 = { No = 0; Edges = Map [Bottom, "###"]}
+                let t2 = { No = 0; Edges = Map [Top,  "###"]}
+                let t3 = { No = 0; Edges = Map [Top,  "##."]}
+                fitTilesV t1 t2 =! true
+                fitTilesV t1 t3 =! false }
+            test "horizontal" {
+                let t1 = { No = 0; Edges = Map [Right, "###"]}
+                let t2 = { No = 0; Edges = Map [Left,  "###"]}
+                let t3 = { No = 0; Edges = Map [Left,  "##."]}
+                fitTilesH t1 t2 =! true
+                fitTilesH t1 t3 =! false }
+        ]
+    ]
